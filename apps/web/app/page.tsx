cb61bbefeffe36f897836c18dba4c1d088d99715
Jose <jloaiza.95@outlook.es>
2026-06-15 14:26:36 -0600

    Add Next.js app scaffold with custom homepage

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+  <h1>Learning how to chambiar</h1>
+  <p>Chamba para no chambiar en call center</p>
+</main>
+  );
+}
